3836dfeeba216a81635c599d3abe81ee3b263a39
Yevhen Bakhtizin <eugenbah@gmail.com>
2023-11-10 00:52:31 +0200

    add individual product screen

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { Expand, ShoppingCart } from "lucide-react";
 
 import { Product } from "@/types";
@@ -12,8 +13,16 @@ interface ProductCard {
 }
 
 const ProductCard: React.FC<ProductCard> = ({ data }) => {
+	const router = useRouter();
+
+	const handleClick = () => {
+		router.push(`/product/${data?.id}`);
+	};
 	return (
-		<div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+		<div
+			onClick={handleClick}
+			className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4"
+		>
 			<div className="aspect-square rounded-xl bg-gray-100 relative">
 				<Image
 					src={data?.images?.at(0)?.url ?? ""}
