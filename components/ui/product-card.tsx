cffa91a26aa5a436605a3bebb1700159b71de9a2
Yevhen Bakhtizin <eugenbah@gmail.com>
2023-11-12 00:54:46 +0200

    add a shopping cart page

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -6,15 +6,19 @@ import { useRouter } from "next/navigation";
 import { Expand, ShoppingCart } from "lucide-react";
 
 import { Product } from "@/types";
+
 import IconButton from "@/components/ui/icon-button";
 import Currency from "@/components/ui/currency";
+
 import usePreviewModal from "@/hooks/use-preview-modal";
+import useCart from "@/hooks/use-cart";
 
 interface ProductCard {
 	data: Product;
 }
 
 const ProductCard: React.FC<ProductCard> = ({ data }) => {
+	const cart = useCart();
 	const previewModal = usePreviewModal();
 	const router = useRouter();
 
@@ -26,6 +30,12 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
 		event.stopPropagation();
 
 		previewModal.onOpen(data);
+	};
+
+	const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+		event.stopPropagation();
+
+		cart.addItem(data);
 	};
 
 	return (
@@ -47,7 +57,7 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
 							icon={<Expand size={20} className="text-gray-600" />}
 						/>
 						<IconButton
-							onClick={() => {}}
+							onClick={onAddToCart}
 							icon={<ShoppingCart size={20} className="text-gray-600" />}
 						/>
 					</div>
